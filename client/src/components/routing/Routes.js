b96d5c6479d493fa7617cc2a68a1d32c917ee097
Pedro Leal <pedrohb88@hotmail.com>
2021-01-17 21:08:31 -0300

    delete unused components

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -5,13 +5,6 @@ import Alert from '../layout/Alert';
 import Login from '../auth/Login';
 import Register from '../auth/Register';
 import Dashboard from '../dashboard/Dashboard';
-import ProfileForm from '../profile-forms/ProfileForm';
-import AddExperience from '../profile-forms/AddExperience';
-import AddEducation from '../profile-forms/AddEducation';
-import Profiles from '../profiles/Profiles';
-import Profile from '../profile/Profile';
-import Posts from '../posts/Posts';
-import Post from '../post/Post';
 
 import College from '../College/College';
 
@@ -27,16 +20,7 @@ const Routes = () => {
 				<Route exact path="/instituicao/:id" component={College} />
 				<Route exact path="/register" component={Register} />
 				<Route exact path="/login" component={Login} />
-				<Route exact path="/profiles" component={Profiles} />
-				<Route exact path="/profile/:id" component={Profile} />
 				<PrivateRoute exact path="/dashboard" component={Dashboard} />
-				<PrivateRoute exact path="/create-profile" component={ProfileForm} />
-				<PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-				<PrivateRoute exact path="/add-experience" component={AddExperience} />
-				<PrivateRoute exact path="/add-education" component={AddEducation} />
-				<PrivateRoute exact path="/posts" component={Posts} />
-				<PrivateRoute exact path="/posts/:id" component={Post} />
-
 				<Route component={NotFound}></Route>
 			</Switch>
 		</section>
